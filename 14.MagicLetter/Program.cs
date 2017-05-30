8cd6f2e95d0ed6773d499feee84a409f688b7cbe
DelKrasimir <dj.kraso.di@gmail.com>
2017-05-30 18:21:12 +0300

    Add project files.

---
+++ 14.MagicLetter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _14.MagicLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstLetter = char.Parse(Console.ReadLine());
+            var secondLetter = char.Parse(Console.ReadLine());
+            var omit = char.Parse(Console.ReadLine());
+            var result = new List<string>();
+
+            for (char i = firstLetter; i <= secondLetter; i++)
+            {
+                for (char j = firstLetter; j <= secondLetter; j++)
+                {
+                    for (char k = firstLetter; k <= secondLetter; k++)
+                    {
+                        result.Add(i.ToString() + j.ToString() + k.ToString());
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", result.Where(x => !x.Contains(omit))));
+
+        }
+    }
+}
---
+++ 7.CakeIngredients/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7BakingIngredients
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int counter = 0;
+
+            for (int i = 1; i <= 21; i++)
+            {
+
+                var ingredient = Console.ReadLine();
+
+                if (ingredient == "Bake!")
+                {
+                    Console.WriteLine($"Preparing cake with {counter} ingredients.");
+                    break;
+                }
+
+                counter++;
+                Console.WriteLine($"Adding ingredient {ingredient}.");
+            }
+
+        }
+    }
+}
